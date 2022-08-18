141f2b7d20edbf5d573692d01156a15a222dda69
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2022-08-18 00:29:06 -0700

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int cnt = 0;
+
+int main() {
+	scanf("%d", &cnt);
+	for (int i = 0; i < cnt; i++) {
+		for (int j = cnt - (i + 1); j > 0; j--) {
+			printf("%s", " ");
+		}
+		for (int j = 0; j < i + 1; j++) {
+			printf("%s", "*");
+		}
+		printf("\n");
+	}
+}
