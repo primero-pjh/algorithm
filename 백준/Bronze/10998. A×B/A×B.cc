5d7ce3c7e2bad582db3655877d579facff66f044
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2022-08-31 23:34:56 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,15 @@
+//2022-08-31
+#include <stdio.h>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d", a*b);
+    
+
+    return 0;
+}
