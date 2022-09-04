6727e61e2686919476db271388be9517a0b7bc9c
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2022-09-04 21:38:12 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,13 @@
+//2022-08-31
+#include <stdio.h>
+#include <vector>
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a + b);
+	printf("%d\n", a - b);
+	printf("%d\n", a * b);
+	printf("%d\n", a / b);
+	printf("%d\n", a % b);
+	return 0;
+}
