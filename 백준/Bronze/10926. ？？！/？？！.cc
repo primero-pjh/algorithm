beca694b23edd9adae123e720f68249147e5e925
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2022-09-20 18:55:57 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	cout << str + "??!" << endl;
+}
