586d536eef9f53a86869fd2e78d7e0720714b3d8
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-06-30 16:25:30 +0900

    [Silver V] Title: 배열 합치기, Time: 652 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> v;
+
+int main() {
+	int n, m;
+	cin.sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	for (int i = 0; i < m; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	return 0;
+}
