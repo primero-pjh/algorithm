435fe1595b3669e798ccc5afa8ae4c6b7a6cca1f
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-14 19:04:14 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,24 @@
+//2023.07.03
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+
+	for (int i = -999; i <= 999; i++) {
+		for (int j = -999; j <= 999; j++) {
+			if ((a * i) + (b * j) == c) {
+				if ((d * i) + (e * j) == f) {
+					cout << i << " " << j << "\n";
+					return 0;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
