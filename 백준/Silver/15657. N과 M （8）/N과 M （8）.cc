12ecdc1883422b23fea9ee1a6205652fe9a013c4
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-16 02:29:12 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int arr[10] = { 0, };
+int n, m;
+void dfs(int start_idx, vector<int> v) {
+	if (v.size() == m) {
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = start_idx; i < n; i++) {
+		v.push_back(arr[i]);
+		dfs(i, v);
+		v.pop_back();
+	}
+}
+
+int main() {
+	cin.sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	vector<int> v;
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		v.push_back(arr[i]);
+		dfs(i, v);
+		v.pop_back();
+	}
+	return 0;
+}
