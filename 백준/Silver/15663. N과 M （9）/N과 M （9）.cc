d5b4ad3c442740959a0bbf1efcbc407ee13ee364
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-17 18:15:24 +0900

    [Silver II] Title: N과 M (9), Time: 40 ms, Memory: 3612 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+
+using namespace std;
+int arr[10] = { 0, };
+int type[10];
+set<string> s;
+int n, m;
+
+void dfs(int idx, vector<int> list) {
+	if (list.size() == m) {
+		string new_str = "";
+		for (int i = 0; i < list.size(); i++) {
+			new_str += to_string(list[i]);
+			new_str += " ";
+		}
+		if (s.find(new_str) == s.end()) {
+			s.insert(new_str);
+			for (int i = 0; i < list.size(); i++) {
+				cout << list[i] << " ";
+			}
+			cout << "\n";
+		}
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (type[i] == 1) { continue; }
+		list.push_back(arr[i]);
+		type[i] = 1;
+		dfs(i, list);
+		list.pop_back();
+		type[i] = 0;
+	}
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		type[i] = 1;
+		v.push_back(arr[i]);
+		dfs(i, v);
+		v.pop_back();
+		type[i] = 0;
+	}
+	return 0;
+}
