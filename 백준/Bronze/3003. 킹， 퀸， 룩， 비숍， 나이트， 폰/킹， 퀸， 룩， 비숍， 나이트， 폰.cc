329886818e6d358e7c7fdf51d5e1b920237369a0
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-19 19:39:57 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a = 1;
+	int b = 1;
+	int c = 2;
+	int d = 2;
+	int z = 2;
+	int f = 8;
+
+	int q, w, e, r, t, y;
+	cin >> q >> w >> e >> r >> t >> y;
+	cout << (a - q) << " " << b - w << " " << c - e << " " << d - r << " " << z - t << " " << f - y << "\n";
+	return 0;
+}
