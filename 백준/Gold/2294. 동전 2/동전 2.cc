1cd2cb32721a6be1095417e1ced947cf24d7198b
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-22 00:40:21 +0900

    [Gold V] Title: 동전 2, Time: 8 ms, Memory: 5968 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -1,63 +1,66 @@
-#include <stdio.h>
+#include <iostream>
+#include <climits>
 #include <algorithm>
-#include<limits.h>
 
 using namespace std;
+int arr[101], dp[101][10001];
 
-int arr[101] = { 0, };
-int dp[101][10001] = { 0, };
 int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
 	int n, k;
-	scanf("%d %d", &n, &k);
+	cin >> n >> k;
 	for (int i = 1; i <= n; i++) {
-		scanf("%d", &arr[i]);
+		cin >> arr[i];
 	}
-	
-	sort(arr, arr + n+1);
+	sort(arr + 1, arr + n + 1);
 	for (int i = 1; i <= n; i++) {
+		int item = arr[i];
+		dp[i][0] = 1;
 		for (int j = 1; j <= k; j++) {
-			if (i != 1) {
+			if (j >= item) {
+				if (j % item == 0) {
+					dp[i][j] = j / item;
+				}
+				else {
+					if (dp[i][j - item] == 0) {
+						dp[i][j] = dp[i - 1][j];
+					}
+					else {
+						if (dp[i - 1][j] != 0) {
+							dp[i][j] = min(dp[i - 1][j], dp[i][j - item] + dp[i][item]);
+						}
+						else {
+							dp[i][j] = (dp[i][j - item] + dp[i][item]);
+						}
+					}
+				}
+			}
+			else {
 				dp[i][j] = dp[i - 1][j];
 			}
-			if (j % arr[i] == 0) {
-				dp[i][j] = j / arr[i];
-			}
-		}
-	}
-
-	for (int i = 2; i <= k; i++) {
-		int m_v = 0;
-		if (dp[n][i] == 0) {
-			m_v = INT_MAX;
-		}
-		else {
-			m_v = dp[n][i];
-		}
-		
-		int a = 1;
-		int b = i - 1;
-		while (a<=b) {
-			if (dp[n][a] != 0 && dp[n][b] != 0) {
-				m_v = min(m_v, (dp[n][a] + dp[n][b]));
-				dp[n][i] = m_v;
-			}
-			a++;
-			b--;
 		}
 	}
 
 	/*for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= k; j++) {
-			printf("%d ", dp[i][j]);
+			cout << dp[i][j] << " ";
 		}
-		printf("\n");
+		cout << "\n";
 	}*/
-	
-	if (dp[n][k] == 0) {
-		printf("%d\n", -1);
+
+	int min_value = INT_MAX;
+	for (int i = 1; i <= n; i++) {
+		if (dp[i][k] != 0) {
+			min_value = min(min_value, dp[i][k]);
+		}
+	}
+	if (min_value == INT_MAX) {
+		cout << -1 << "\n";
 	}
 	else {
-		printf("%d", dp[n][k]);
+		cout << min_value << "\n";
 	}
 	return 0;
 }
