8f85e8a2e5cdfc3b7aef3c928491d5eb5b704cdf
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-21 15:38:58 +0900

    [Silver IV] Title: Byte Coin, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17521. Byte Coin/Byte Coin.cc
+++ 백준/Silver/17521. Byte Coin/Byte Coin.cc
@@ -1,58 +1,24 @@
 #include <iostream>
 
 using namespace std;
-
-long long arr[16];
-int type[16];	// 1: 매수 -1: 매도 0: 보유
+typedef long long ll;
+ll n, w, arr[16], coin;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	// 날짜 수, 초기 현금
-	int n;
-	long long w;	
 	cin >> n >> w;
 	for (int i = 0; i < n; i++) {
 		cin >> arr[i];
 	}
 
-	for (int i = 0; i < n; i++) {
-		if (i == 0) {
-			if (arr[i] < arr[i + 1]) {
-				type[i] = 1;
-			}
-		}
-		else if (i == n - 1) {
-			if (arr[i - 1] < arr[i]) {
-				type[i - 1] = 1;
-			}
-			type[i] = -1;
-		}
-		else {
-			if (arr[i] > arr[i - 1] && arr[i] >= arr[i + 1]) {
-				type[i] = -1;
-			}
-			if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1]) {
-				type[i] = 1;
-			}
-			if (arr[i - 1] < arr[i]) {
-				type[i - 1] = 1;
-			}
+	for (int i = 0; i < n - 1; i++) {
+		if (arr[i] < arr[i + 1]) {
+			coin = (w / arr[i]);
+			w = w % arr[i];
+			w = w + (arr[i + 1] * coin);
 		}
 	}
 
-	long long coin = 0;
-
-	for (int i = 0; i < n; i++) {
-		if (type[i] == 1) {
-			coin = coin + (w / arr[i]);
-			w = w - (w / arr[i]) * arr[i];
-		}
-		else if (type[i] == -1) {
-			w = w + (coin * arr[i]);
-			coin = 0;
-		}
-	}
-
-	cout << w + (coin * arr[n - 1]);
+	cout << w << "\n";
 	return 0;
 }
