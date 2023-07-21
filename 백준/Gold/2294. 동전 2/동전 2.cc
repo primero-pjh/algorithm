f0d6dbd87855389f5d45d4263f354af4c84a6440
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-22 00:49:47 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.cc
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -3,7 +3,7 @@
 #include <algorithm>
 
 using namespace std;
-int arr[101], dp[101][10001];
+int arr[101], dp[10001];
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -11,56 +11,20 @@ int main() {
 
 	int n, k;
 	cin >> n >> k;
+	for (int i = 1; i <= k; i++) {
+		dp[i] = 10001;
+	}
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
-	}
-	sort(arr + 1, arr + n + 1);
-	for (int i = 1; i <= n; i++) {
-		int item = arr[i];
-		dp[i][0] = 1;
-		for (int j = 1; j <= k; j++) {
-			if (j >= item) {
-				if (j % item == 0) {
-					dp[i][j] = j / item;
-				}
-				else {
-					if (dp[i][j - item] == 0) {
-						dp[i][j] = dp[i - 1][j];
-					}
-					else {
-						if (dp[i - 1][j] != 0) {
-							dp[i][j] = min(dp[i - 1][j], dp[i][j - item] + dp[i][item]);
-						}
-						else {
-							dp[i][j] = (dp[i][j - item] + dp[i][item]);
-						}
-					}
-				}
-			}
-			else {
-				dp[i][j] = dp[i - 1][j];
-			}
+		for (int j = arr[i]; j <= k; j++) {
+			dp[j] = min(dp[j], dp[j - arr[i]] + 1);
 		}
 	}
-
-	/*for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= k; j++) {
-			cout << dp[i][j] << " ";
-		}
-		cout << "\n";
-	}*/
-
-	int min_value = INT_MAX;
-	for (int i = 1; i <= n; i++) {
-		if (dp[i][k] != 0) {
-			min_value = min(min_value, dp[i][k]);
-		}
-	}
-	if (min_value == INT_MAX) {
+	if (dp[k] == 10001) {
 		cout << -1 << "\n";
 	}
 	else {
-		cout << min_value << "\n";
+		cout << dp[k];
 	}
 	return 0;
 }
