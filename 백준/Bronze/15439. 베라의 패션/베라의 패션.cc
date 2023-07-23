99593fddb402ee5d666c71b5c63271db5a242111
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-23 18:15:55 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	int n;
+	cin >> n;
+	cout << n * (n - 1) << "\n";
+}
