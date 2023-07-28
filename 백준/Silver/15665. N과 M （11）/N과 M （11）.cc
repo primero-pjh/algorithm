225d51dc22e67317a502311456fa105dd8ca1c20
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-07-28 12:12:47 +0900

    [Silver II] Title: N과 M (11), Time: 508 ms, Memory: 26844 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[8];
+set<string> s;
+void dfs(vector<int> v) {
+	if (v.size() == m) {
+		string str = "";
+		for (int i = 0; i < v.size(); i++) {
+			str += to_string(v[i]) + " ";
+		}
+		if (s.find(str) != s.end()) {
+			return;
+		}
+		else {
+			s.insert(str);
+			cout << str << "\n";
+		}
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		v.push_back(arr[i]);
+		dfs(v);
+		v.pop_back();
+	}
+}
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	vector<int> v;
+	sort(arr, arr + n);
+	for (int i = 0; i < n; i++) {
+		v.push_back(arr[i]);
+		dfs(v);
+		v.pop_back();
+	}
+	return 0;
+}
