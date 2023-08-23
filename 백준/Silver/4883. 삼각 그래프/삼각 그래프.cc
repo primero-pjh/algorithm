8058252758225b1f39a6a7fee15d932c07994dd3
PRITRAS <58695375+primero-pjh@users.noreply.github.com>
2023-08-23 20:47:52 +0900

    [Silver I] Title: 삼각 그래프, Time: 32 ms, Memory: 8272 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.cc
@@ -1,69 +1,51 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <climits>
+#include <string.h>
+
 using namespace std;
 typedef long long ll;
 
-//백준 삼각 그래프 4883
-
-ll min(ll a, ll b) {
-    if (a < b) {
-        return a;
-    }
-    else {
-        return b;
-    }
-}
-
-int main(void) {
-    int first = 1;
-
-    while (1) {
-        int n;
-        cin >> n;
-        if (n == 0) {
-            break;
-        }
-        ll dp[100001][3];
-        int arr[100001][3];
-
-        for (int i = 0;i < n;i++) {
-            for (int j = 0;j < 3;j++) {
-                cin >> arr[i][j];
-
-            }
-        }
-
-
-        dp[0][0] = 987654321;
-        dp[0][1] = arr[0][1];
-        dp[0][2] = dp[0][1] + arr[0][2];
-
-
-
-
-        for (int i = 1;i < n;i++) {
-            for (int j = 0;j < 3;j++) {
-                if (j == 0) {
-                    dp[i][j] = min(dp[i - 1][j] + arr[i][j], dp[i - 1][j + 1] + arr[i][j]);
-                }
-                else if (j == 1) {
-                    ll first_path = dp[i][j - 1] + arr[i][j];
-                    ll second_path = dp[i - 1][j - 1] + arr[i][j];
-                    ll third_path = dp[i - 1][j] + arr[i][j];
-                    ll last_path = dp[i - 1][j + 1] + arr[i][j];
-                    ll temp = min(first_path, second_path);
-                    dp[i][j] = min(last_path, (min(temp, third_path)));
-                }
-                else if (j == 2) {
-                    ll first_path = dp[i - 1][j] + arr[i][j];
-                    ll second_path = dp[i - 1][j - 1] + arr[i][j];
-                    ll third_path = dp[i][j - 1] + arr[i][j];
-                    dp[i][j] = min(third_path, min(first_path, second_path));
-                }
-            }
-        }
-
-
-        cout << first << '.' << ' ' << dp[n - 1][1] << '\n';
-        first++;
-    }
+int n;
+ll arr[100001][4];
+ll dp[100001][4];
+int dx[4] = { -1, -1, -1, 0 };
+int dy[4] = { -1, 0, 1, -1 };
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int idx = 1;
+	while (cin >> n) {
+		if (n == 0) {
+			exit(0);
+		}
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= 3; j++) {
+				cin >> arr[i][j];
+				dp[i][j] = 1000000000;
+			}
+		}
+		//dp[1][1] = arr[1][1];
+		dp[1][2] = arr[1][2];
+		dp[1][3] = (arr[1][2] + arr[1][3]);
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= 3; j++) {
+				for (int k = 0; k < 4; k++) {
+					int nx = i + dx[k];
+					int ny = j + dy[k];
+					if (nx <= 0 || ny <= 0 || nx > n || ny > 3) { continue; }
+					dp[i][j] = min(dp[i][j], dp[nx][ny] + arr[i][j]);
+				}
+			}
+		}
+		/*for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= 3; j++) {
+				cout <<  dp[i][j] << " ";
+			}
+			cout << "\n";
+		}*/
+		cout << idx << ". " << dp[n][2] << "\n";
+		idx++;
+	}
+	return 0;
 }
